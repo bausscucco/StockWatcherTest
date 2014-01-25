effac3c286792291627d1f0e511366dc1dc0f536
unknown <zankyou0609@hotmail.com>
2014-01-24 21:17:22 -0800

    lalala

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -25,9 +25,7 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.Anchor;
 
-// oh man I added a comment better commit this right away
-
-// man...I hate eclipse....wont show my pretty mojis....
+// need some sleep atm....
 
 
 public class StockWatcher implements EntryPoint {
